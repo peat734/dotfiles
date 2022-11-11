5a4f270234956a57e59df22582b6046c954f4489
peat <94827899+peat734@users.noreply.github.com>
2022-11-11 23:42:12 +0100

    saga color scheme

--- dwm/config.h
+++ dwm/config.h
@@ -8,14 +8,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10", "fontawesome:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#2A2A37";
-static const char col_gray2[]       = "#16161D";
-static const char col_gray3[]       = "#C8C093";
+static const char col_gray1[]       = "#0A0D0F";
+static const char col_gray2[]       = "#141719";
+static const char col_gray3[]       = "#0f1214";
 static const char col_gray4[]       = "#1F1F28";
 static const char col_cyan[]        = "#005577";
 static const char col_red[]         = "#e78c8c";
-static const char col_pink[]        = "#D27E99";
-static const char col_white[]       = "#DCD7BA";
+static const char col_pink[]        = "#ffc2df";
+static const char col_white[]       = "#ffe1e1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_white, col_gray1, col_gray2 },
@@ -61,27 +61,30 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_pink, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 
 #include "shiftview.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY|ShiftMask,             XK_w,      spawn,          SHCMD("firefox")},
-  { MODKEY,                       XK_m,      spawn,          SHCMD("rofi -modi drun,run -show drun")},
-  { MODKEY,                       XK_Print,  spawn,          SHCMD("flameshot gui")},
+    { MODKEY|ShiftMask,             XK_w,      spawn,          SHCMD("firefox")},
+    { MODKEY,                       XK_m,      spawn,          SHCMD("rofi -modi drun,run -show drun")},
+    { MODKEY,                       XK_Print,  spawn,          SHCMD("flameshot gui")},	
+    { ControlMask|Mod1Mask,         XK_l,      spawn,          SHCMD("slock")},
+	//{ NULL,                         XF86XK_AudioLowerVolume,     spawn,          SHCMD("pactl set-sink-volume 0 -5%")},
+    //{ NULL,                         XF86XK_AudioRaiseVolume,     spawn,          SHCMD("pactl set-sink-volume 0 +5%")},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
-  { ControlMask|Mod1Mask,         XK_Right,  shiftview,      {.i = +1 } },
-  { ControlMask|Mod1Mask,         XK_Left,   shiftview,      {.i = -1 } },
+    { ControlMask|Mod1Mask,         XK_Right,  shiftview,      {.i = +1 } },
+    { ControlMask|Mod1Mask,         XK_Left,   shiftview,      {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-  { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+    { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
